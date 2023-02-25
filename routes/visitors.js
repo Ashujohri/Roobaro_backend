84d5229b444e055ad53e4edf6b88f43e2b657e84
Ashutosh <ashutosh@plus91labs.com>
2023-02-25 12:19:45 +0530

    commit for test again

--- routes/visitors.js
+++ routes/visitors.js
@@ -2,19 +2,5 @@ var express = require("express");
 var router = express.Router();
 var pool = require("../dbconfig/pool");
 
-router.get("/display", function (req, res, next) {
-  pool.query("select * from visitors;", function (error, result) {
-    if (error) {
-      console.log("error in visitor", error);
-      return res
-        .status(400)
-        .json({ status: 400, message: "Bad request", error });
-    } else {
-      return res
-        .status(200)
-        .json({ status: true, message: "result found", result });
-    }
-  });
-});
 
 module.exports = router;
